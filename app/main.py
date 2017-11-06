9d9a3f5cd9a75676fe5c5ca97f73a149857c8969
Arsen Losenko <arsenlosenko@gmail.com>
2017-11-06 22:30:47 +0200

    Enabled start of flask from main.py file

---
+++ app/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from server import socket
+
+if __name__ == "__main__":
+    socket.start_socket()
--- app/server/socket.py
+++ app/server/socket.py
@@ -1,12 +1,14 @@
 
 import os
-from flask import Flask
+import jinja2 
+from flask import Flask, render_template 
 from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.config["DEBUG"] = True
+app.jinja_loader = jinja2.FileSystemLoader(os.getcwd() + '/client/src/')
 
 socketio = SocketIO(app)
 app.config.update(
@@ -15,13 +17,20 @@ app.config.update(
     TEMPLATES_AUTO_RELOAD=True
 )
 
+
+@app.route('/')
+def index():
+    print("Entered")
+    return render_template('index.html')
+
+
 @socketio.on('connect')
 def handle_connect(data):
     print("Received data:", data)
     socketio.emit('response', data)
 
 
-if __name__ == "__main__":
+def start_socket():
     print("Starting app...")
     socketio.run(app)
 
